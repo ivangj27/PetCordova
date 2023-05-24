5dc8a9443bb24fbd68cbed77983dc1d3f0be03d1
Fran <farmariobocanegra@safareyes.es>
2023-05-24 10:01:21 +0200

    añadir campo fecha a informacion mascota

--- www/js/informacionMascota.js
+++ www/js/informacionMascota.js
@@ -46,15 +46,16 @@ import {
     botonAdoptar.textContent = "Adoptar"
     divBotonAdoptar.appendChild(botonAdoptar);
     ventanaPrincipal.appendChild(divBotonAdoptar);
-    //Nombre + edad
-    console.log(pet.nacimiento);
-    var cumple_array = pet.nacimiento.split("/");
-    var cumple_date = new Date(cumple_array[2], cumple_array[1] - 1, cumple_array[0]);
-    var edadDiff = Date.now() - cumple_date.getTime();
-    var edadDate = new Date(edadDiff);
-    const edadMascota = Math.abs(edadDate.getUTCFullYear() - 1970);
+
     const divDatosMascota = document.createElement("div");
     divDatosMascota.classList.add("divDatosMascota");
+
+    //Nombre + edad
+    var cumple_array = pet.nacimiento.split("/"); // Divido la fecha
+    var cumple_date = new Date(cumple_array[2], cumple_array[1] - 1, cumple_array[0]); // creo el Date con la fecha dividida
+    var edadDiff = Date.now() - cumple_date.getTime(); 
+    var edadDate = new Date(edadDiff); // transformo la diferencia en Date
+    const edadMascota = Math.abs(edadDate.getUTCFullYear() - 1970); // para luego calcular la edad
     const divContenedorNombre = document.createElement("div");
     divContenedorNombre.classList.add("inputContainer");
     const divContenedorEdad = document.createElement("div");
@@ -72,10 +73,10 @@ import {
     edadInput.setAttribute("type", "text");
     edadInput.setAttribute("placeholder", "a");
     edadInput.classList.add("camposTextoDatosMascota");
-    //edadInput.setAttribute("readonly","");
+    edadInput.setAttribute("readonly","");
     nombreInput.setAttribute("type", "text");
     nombreInput.classList.add("camposTextoDatosMascota");
-    //nombreInput.setAttribute("readonly","");
+    nombreInput.setAttribute("readonly","");
     nombreInput.setAttribute("placeholder", "a");
     nombreInput.value = pet.nombre;
     edadInput.value = edadMascota;
@@ -87,7 +88,15 @@ import {
     divDatosMascota.appendChild(divContenedorEdad);
     ventanaPrincipal.appendChild(divDatosMascota);
 
-    //
+    //FECHA NACIMIENTO MASCOTA
+    const divContenedorFechaNacimiento = document.createElement("div");
+    divContenedorFechaNacimiento.classList.add("inputContainer");
+    const fechaNacimientoInput = document.createElement("input");
+    fechaNacimientoInput.classList.add("camposTextoDatosMascota");
+    fechaNacimientoInput.setAttribute("type","datetime-local");
+    fechaNacimientoInput.setAttribute("placeholder", "a");
+    fechaNacimientoInput.setAttribute("readonly", "");
+    
 
 
     appWindow.appendChild(ventanaPrincipal);
